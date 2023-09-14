1f3b63e1a9fb09cc57ed5c34380cf08b32b9326f
Bryanna Boone <bryannaboone@wireless-10-155-219-230.public.utexas.edu>
2023-09-14 16:23:26 -0500

    some commit

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -1,3 +1,7 @@
+
+# This program computes the area of a circle
+
+
 def compute_area_of_circle(radius):
 	pi = 3.14
 	area = pi * radius * radius
@@ -5,8 +9,8 @@ def compute_area_of_circle(radius):
 
 radius1 = 30
 area1 = compute_area_of_circle(radius1)
-print(f"The area of circle with radius {radius1} is: {area1}")
+print("The area of circle with radius", radius1," is:", area1)
 
 radius2 = 40
 area2 = compute_area_of_circle(radius2)
-print(f"The area of circle with radius {radius2} is: {area2}")
+print("The area of circle with radius", radius2," is:", area2)
